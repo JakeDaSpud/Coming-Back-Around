1e18411ae3eb33b1b73a059b74f4b1f6837b149f
jake o'reilly <jor.gamedev@gmail.com>
2026-06-03 13:49:37 +0100

    :sparkles: finished page tech, moving onto storyboarding

--- script.js
+++ script.js
@@ -18,29 +18,7 @@ class Page
 }
 
 var Pages = new Map();
-Pages.set("home", new Page(
-    "home", "Home",
-    "normalp1", 10000, "nextlingertest",
-    null, null, null,
-    '<img src="./assets/home-blurb.png"><p>Coming Back Around is a story about a Kid and a Nutcracker.</p>'));
-
-Pages.set("normalp1", new Page(
-    "normalp1", "1",
-    null, null, null,
-    "home", null, null,
-    '<p>page 1</p>'));
-
-Pages.set("nextlingertest", new Page(
-    "nextlingertest", "next_test Title",
-    null, null, null,
-    "home", 5000, "evilhome",
-    '<p>waited 10 secs and went forward</p>'));
-
-Pages.set("evilhome", new Page(
-    "evilhome", "EVILLLLLlll",
-    "normalp1", null, null,
-    null, null, null,
-    '<p>EVILL HOMEE >:))))</p>'));
+addAllPages(Pages)
 
 var pageNumber = document.getElementById("page-number");
 var nextButton = document.getElementById("next");
@@ -49,6 +27,83 @@ var pageContainer = document.getElementById("page-container");
 var current_page = null;
 var page_loaded_timestamp = null;
 setPage("home");
+
+
+function addAllPages(PagesMap)
+{
+    // left
+    // image
+        // class="d-block align-self-start"
+    // text
+        // class="align-self-start"
+
+    // center
+    // image
+        // class="d-block align-self-center"
+    // text
+        // class="align-self-center"
+
+    // right
+    // image
+        // class="d-block align-self-end"
+    // text
+        // class="align-self-end"
+
+
+    PagesMap.set("home", new Page(
+        "home", "Home",
+        "1", 10000, "nextlingertest",
+        null, null, null,
+        '<img src="./assets/home-blurb.png" class="d-block align-self-center"><p class="align-self-center">Coming Back Around is a story about a Kid and a Nutcracker.</p>'));
+
+    PagesMap.set("1", new Page(
+        "1", "1",
+        "2", null, null,
+        "home", null, null,
+        '<img src="" class="d-block align-self-center">' +
+        '<p class="align-self-center">What do I do now?</p>'));
+
+    PagesMap.set("2", new Page(
+        "2", "2 - Align testing",
+        "3", null, null,
+        "1", null, null,
+        '<p>length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length-length</p>' +
+        
+        '<img src="./assets/home-blurb.png" class="d-block align-self-start">' +
+        '<p class="align-self-start">this is the first piece of text on the left</p>' +
+        
+        '<img src="./assets/home-blurb.png" class="align-self-center">' +
+        '<p class="align-self-center">this is the second piece of text in the centre</p>' +
+        
+        '<img src="./assets/home-blurb.png" class="align-self-end">' +
+        '<p class="align-self-end">this is the third piece of text on the right</p>' +
+
+        '<img src="./assets/home-blurb.png" class="d-block align-self-start">' +
+        '<p class="align-self-start">this is the first piece of text on the left</p>' +
+        
+        '<img src="./assets/home-blurb.png" class="align-self-end">' +
+        '<p class="align-self-end">this is the third piece of text on the right</p>' 
+        
+    ));
+
+    PagesMap.set("3", new Page(
+        "3", "3 - Right Align Test",
+        null, null, null,
+        "2", null, null,
+        null));
+
+    PagesMap.set("nextlingertest", new Page(
+        "nextlingertest", "next_test Title",
+        null, null, null,
+        "home", 5000, "evilhome",
+        '<p>waited 10 secs and went forward</p>'));
+
+    PagesMap.set("evilhome", new Page(
+        "evilhome", "EVILLLLLlll",
+        "normalp1", null, null,
+        null, null, null,
+        '<p>EVILL HOMEE >:))))</p>'));
+}
 
 
 function setPage(page_id)
@@ -60,7 +115,10 @@ function setPage(page_id)
     current_page = Pages.get(page_id);
 
     // Add content to page container
-    pageContainer.innerHTML = current_page.story_content;
+    if (current_page.story_content == null)
+        pageContainer.innerHTML = '<h1>error, no current_page.story_content for id[' + current_page.id + ']</h1>';
+    else
+        pageContainer.innerHTML = current_page.story_content;
 
     // Set Buttons
     nextButton.disabled = current_page.next == null ? true : false;
